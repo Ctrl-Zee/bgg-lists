e46742611e31a7b8ea870f1b23d00020490a5565
andrew.smith <andrew.smith.indy@gmail.com>
2025-02-01 18:47:03 -0500

    fixed tailwind and added layout

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,22 @@
+import { Link } from '@tanstack/react-router';
+import { ReactNode } from 'react';
+
+type RootLayoutProps = {
+  children: ReactNode;
+};
+
+export const RootLayout = ({ children }: RootLayoutProps) => {
+  return (
+    <>
+      <div className="flex h-screen flex-col overflow-hidden bg-red-100">
+        <Link to="/collection" className="[&.active]:font-bold">
+          Collection
+        </Link>
+        <main className="grow overflow-y-auto">
+          {children}
+          {/* <Suspense fallback={<Loading />}>{children}</Suspense> */}
+        </main>
+      </div>
+    </>
+  );
+};
--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -11,10 +11,17 @@
 // Import Routes
 
 import { Route as rootRoute } from './routes/__root'
+import { Route as CollectionImport } from './routes/collection'
 import { Route as AboutImport } from './routes/about'
 import { Route as IndexImport } from './routes/index'
 
 // Create/Update Routes
+
+const CollectionRoute = CollectionImport.update({
+  id: '/collection',
+  path: '/collection',
+  getParentRoute: () => rootRoute,
+} as any)
 
 const AboutRoute = AboutImport.update({
   id: '/about',
@@ -46,6 +53,13 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof AboutImport
       parentRoute: typeof rootRoute
     }
+    '/collection': {
+      id: '/collection'
+      path: '/collection'
+      fullPath: '/collection'
+      preLoaderRoute: typeof CollectionImport
+      parentRoute: typeof rootRoute
+    }
   }
 }
 
@@ -54,36 +68,41 @@ declare module '@tanstack/react-router' {
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
   '/about': typeof AboutRoute
+  '/collection': typeof CollectionRoute
 }
 
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
   '/about': typeof AboutRoute
+  '/collection': typeof CollectionRoute
 }
 
 export interface FileRoutesById {
   __root__: typeof rootRoute
   '/': typeof IndexRoute
   '/about': typeof AboutRoute
+  '/collection': typeof CollectionRoute
 }
 
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/' | '/about'
+  fullPaths: '/' | '/about' | '/collection'
   fileRoutesByTo: FileRoutesByTo
-  to: '/' | '/about'
-  id: '__root__' | '/' | '/about'
+  to: '/' | '/about' | '/collection'
+  id: '__root__' | '/' | '/about' | '/collection'
   fileRoutesById: FileRoutesById
 }
 
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
   AboutRoute: typeof AboutRoute
+  CollectionRoute: typeof CollectionRoute
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
   AboutRoute: AboutRoute,
+  CollectionRoute: CollectionRoute,
 }
 
 export const routeTree = rootRoute
@@ -97,7 +116,8 @@ export const routeTree = rootRoute
       "filePath": "__root.tsx",
       "children": [
         "/",
-        "/about"
+        "/about",
+        "/collection"
       ]
     },
     "/": {
@@ -105,6 +125,9 @@ export const routeTree = rootRoute
     },
     "/about": {
       "filePath": "about.tsx"
+    },
+    "/collection": {
+      "filePath": "collection.tsx"
     }
   }
 }
